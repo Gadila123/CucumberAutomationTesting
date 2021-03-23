32ff24b93c44d13f8175f65e09e724bc215e92a3
Gadila123 <venkateshnani23@gmail.com>
2021-03-23 21:13:27 +0530

    conflict Practice

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "G:\\AutomationPractice\\CucumberFramework\\src\\test\\java\\features\\ParabankCustomerForm.feature", glue = {
-		"stepdefinations" }, monochrome = true, plugin = { "pretty",
+		"stepdefinations" }, monochrome = false, plugin = { "pretty",
 				"json:target/cucumber-json/cucumber.json", "junit:target/cucumber-reports/Cucumber.xml",
 				"html:target/cucumber-reports/Cucumber.html"}, dryRun = false)
 
